9ee6ccc4b6a57cb1427dd637791bea8898680697
canhdeptrai222 <158004919+canhdeptrai222@users.noreply.github.com>
2024-08-07 11:13:00 +0700

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,57 +1,95 @@
-function age(){
-    let x = document.getElementById("age").value;
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    var age = currentYear - x;
-    var name = document.getElementById("name");
-    document.getElementById("ten").innerHTML = "Chào bạn " + name.value + '!';
-    document.getElementById("tuoi").innerHTML = "Tuổi của bạn là: " + age;
+function age() {
+  let x = document.getElementById("age").value;
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  var age = currentYear - x;
+  var name = document.getElementById("name").value;
+  if (name == "") {
+    alert("Tên chưa nhập");
+    return false;
+  }
+  if (isNaN(x) || x == "") {
+    alert("Năm sinh ko hợp lệ");
+    return false;
+  }
+  document.getElementById("res").innerHTML = "Chào bạn " + name + "!<br> Tuổi của bạn là: " + age;
 }
-function calculate(){
-    var a = Number(document.getElementById('num1').value) ;
-    var b = Number(document.getElementById('num2').value);
-    var operation = document.querySelector('input[name="operation"]:checked').value;
-    //var operation = document.getElementById('input[name="operation"]:checked').value;
-    let result;
-    switch (operation) {
-      case '+':
-        result = a + b;
-        break;
-      case '-':
-        result = a - b;
-        break;
-      case '*':
-        result = a * b;
-        break;
-      case '/':
-        result   = a / b;
-        break; 
-  
-    }
-   document.getElementById("result").value = result;
+function calculate() {
+  var a = Number(document.getElementById('num1').value);
+  var b = Number(document.getElementById('num2').value);
+  var operation = document.querySelector('input[name="operation"]:checked').value;
+  //var operation = document.getElementById('input[name="operation"]:checked').value;
+  let result;
+  switch (operation) {
+    case '+':
+      result = a + b;
+      break;
+    case '-':
+      result = a - b;
+      break;
+    case '*':
+      result = a * b;
+      break;
+    case '/':
+      result = a / b;
+      break;
+
+  }
+  document.getElementById("result").value = result;
 }
 
-function calPhuongTrinhBac2(){
-   var a = Number(document.getElementById('varA').value);
-   var b = Number(document.getElementById('varB').value);
-   var c = Number(document.getElementById('varC').value);
-   if(a != 0){
-       var delta = b*b - 4*a*c;
-       if(delta < 0){
-        
-       }
-       else if(delta == 0){
-        var x1 = -b/(2*a);
-        console.log(x1);
-       }
-       else{
-        var x1 = (-b / Math.sqrt(delta)) / (2* a);
-        var x2 = (b / Math.sqrt(delta)) / (2* a);
-       console.log(x1);
-       console.log(x2);
-       }
-   }
+function calPhuongTrinhBac2() {
+  var a = Number(document.getElementById('varA').value);
+  var b = Number(document.getElementById('varB').value);
+  var c = Number(document.getElementById('varC').value);
+  if (a != 0 || a != "" || b != "" || c != "") {
+    var delta = b * b - 4 * a * c;
+    if (delta < 0) {
+      document.getElementById("res").innerHTML = "Phương trình vô nghiệm!";
+    }
+    else if (delta == 0) {
+      var x1 = -b / (2 * a);
+      document.getElementById("res").innerHTML = "Phương trình có nghiệm kép <br> x1 = x2 = " + x1;
+    }
+    else {
+      var x1 = (-b + Math.sqrt(delta)) / (2 * a);
+      var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+      document.getElementById("res").innerHTML = "Phương trình có 2 nghiệm phân biệt:<br> x1 = " + x1.toFixed(2) + "<br> x2 = " + x2.toFixed(2);
+    }
+  } else {
+    alert("Chưa nhập!");
+  }
 }
+
+function Tinh() {
+  var a = Number(document.forms["myForm"]["num1"].value);
+  var b = Number(document.forms["myForm"]["num2"].value);
+  var op = document.forms["myForm"]["operation"].value;
+  var kq;
+  switch (op) {
+    case "+":
+      kq = a + b;
+      break;
+    case "-":
+      kq = a - b;
+      break;
+    case "*":
+      kq = a * b;
+      break;
+    case "/":
+      if (b != 0) {
+        kq = a / b;
+      }
+      else {
+        kq = "Không thể chia cho 0";
+      }
+      break;
+  }
+  document.forms["myForm"]["kq"].value = kq;
+  return false;
+}
+
+
 
 // function writeLog(){
 //     var mystring = '';
